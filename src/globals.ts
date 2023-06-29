aed58c6922f050b9b1fea63bc722b47c0fa6caf5
Christian-Klempau <christian.klempau@uc.cl>
2023-06-29 11:39:12 -0400

    Chile timezone

--- src/globals.ts
+++ src/globals.ts
@@ -1,9 +1,11 @@
 let baseUrl: string;
-baseUrl = "http://lagarto9.ing.puc.cl";
 baseUrl = "http://localhost:3000";
+baseUrl = "http://lagarto9.ing.puc.cl";
 
 function strToLocale(str: string) {
-  return new Date(str).toLocaleString();
+  return new Date(str).toLocaleString("es-ES", {
+    timeZone: "America/Santiago",
+  });
 }
 
 export { baseUrl, strToLocale };
